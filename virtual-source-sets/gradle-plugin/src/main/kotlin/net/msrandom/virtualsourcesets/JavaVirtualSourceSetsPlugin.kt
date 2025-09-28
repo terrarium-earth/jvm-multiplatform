1c296c02889d86e0d8d62d4e824e12267645156d
Ashley Wright <ashley@msrandom.net>
2025-09-29 01:52:21 +0300

    Remove ksp-based `actual` stubs

--- virtual-source-sets/gradle-plugin/src/main/kotlin/net/msrandom/virtualsourcesets/JavaVirtualSourceSetsPlugin.kt
+++ virtual-source-sets/gradle-plugin/src/main/kotlin/net/msrandom/virtualsourcesets/JavaVirtualSourceSetsPlugin.kt
@@ -23,6 +23,7 @@ import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetContainer
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
@@ -76,69 +77,6 @@ open class JavaVirtualSourceSetsPlugin @Inject constructor(private val modelBuil
 
         dependency.extensions.getByType(SourceSetStaticLinkageInfo::class.java).links.all {
             addCommonResources(it, task)
-        }
-    }
-
-    private fun SourceSet.setupKotlinStubs(
-        kotlin: KotlinJvmExtension,
-        taskContainer: TaskContainer,
-        buildDirectory: DirectoryProperty,
-        fileOperations: FileOperations,
-    ) {
-        val compilation = kotlin.target.compilations.getByName(name)
-        val kspTaskName = compilation.compileKotlinTaskName.replace("compile", "ksp")
-
-        val stubsDirectory = buildDirectory.dir("generated").map {
-            val ksp = it.dir("ksp")
-
-            val targetDirectory = if (kotlin.target.name.isEmpty()) {
-                ksp
-            } else {
-                ksp.dir(kotlin.target.name)
-            }
-
-            targetDirectory.dir(name).dir("stubs")
-        }
-
-        fun configureTask(compileTask: KotlinCompile) {
-            if (compileTask.multiplatformStructure.fragments.get().any { it.fragmentName == name }) {
-                return
-            }
-
-            compileTask.multiPlatformEnabled.set(true)
-
-            val kotlinSourceSet = kotlin.sourceSets.getByName(name)
-
-            compileTask.addK2Argument(compileTask.multiplatformStructure.fragments) {
-                K2MultiplatformStructure.Fragment(kotlinSourceSet.name, kotlinSourceSet.kotlin.asFileTree)
-            }
-
-            compileTask.addK2Argument(compileTask.multiplatformStructure.fragments) {
-                K2MultiplatformStructure.Fragment("stub-implementation", fileOperations.fileTree(stubsDirectory))
-            }
-
-            compileTask.addK2Argument(compileTask.multiplatformStructure.refinesEdges) {
-                K2MultiplatformStructure.RefinesEdge("stub-implementation", kotlinSourceSet.name)
-            }
-
-            commonSourceSet.get(compileTask).from(kotlinSourceSet.kotlin.srcDirTrees)
-            compileTask.source(stubsDirectory)
-        }
-
-        taskContainer.withType(KspTaskJvm::class.java).named(kspTaskName::equals).all {
-            // Ksp task exists, setup stub directory to be used if needed
-
-            it.options.add(SubpluginOption("apoption", lazy { "actualStubDir=${stubsDirectory.get()}" }))
-
-            configureTask(it)
-        }
-
-        compilation.compileTaskProvider.configure {
-            configureTask(it as KotlinCompile)
-        }
-
-        extensions.getByType(SourceSetStaticLinkageInfo::class.java).links.all {
-            it.setupKotlinStubs(kotlin, taskContainer, buildDirectory, fileOperations)
         }
     }
 
@@ -210,8 +148,6 @@ open class JavaVirtualSourceSetsPlugin @Inject constructor(private val modelBuil
         project.plugins.withId(KOTLIN_JVM) {
             val kotlin = project.extensions.getByType(KotlinJvmExtension::class.java)
             val kotlinCompilation = kotlin.target.compilations.getByName(name)
-
-            dependency.setupKotlinStubs(kotlin, project.tasks, project.layout.buildDirectory, project.serviceOf())
 
             kotlinCompilation.compileTaskProvider.configure {
                 it as KotlinCompile
